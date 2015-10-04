481e8ac5e4be8575b767cde4f4a9dedc9302a6b5
goncris12 <goncris12@hotmail.com>
2015-10-04 15:42:32 -0500

    Activity Pending

--- app/src/main/java/com/ecoentorno/ecomobile/MainActivityFragment.java
+++ app/src/main/java/com/ecoentorno/ecomobile/MainActivityFragment.java
@@ -1,22 +1,40 @@
 package com.ecoentorno.ecomobile;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private Button btBilling;
+
     public MainActivityFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
+        btBilling = (Button) view.findViewById(R.id.btBilling);
+        btBilling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), getString(R.string.billing), Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getActivity(),MainBillingActivity.class));
+            }
+        });
+
+        return view;
+
+
     }
 }
